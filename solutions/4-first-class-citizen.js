5fd8f5dfc2194ec1dd2f5cd7ef10774e5c1e6eee
dima55279 <dima55279@yandex.ru>
2025-03-21 20:30:48 +0300

    Выполнено задание 4.

--- solutions/4-first-class-citizen.js
+++ solutions/4-first-class-citizen.js
@@ -1,9 +1,13 @@
 const run = (text) => {
     // BEGIN
-
+    const takeLast = (text, len) => {
+        if (text.length >= 4) {
+            return text.split("").reverse().join("").slice(0, len);
+        }
+        return null;
+    }
     // END
   
     return takeLast(text, 4);
   };
-  
 export default run;
