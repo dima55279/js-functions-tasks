90d58a66bd02bd69149f5349bb1b29de18f6d195
dima55279 <dima55279@yandex.ru>
2025-03-21 21:06:23 +0300

    Выполнено задание 7.

--- solutions/7-filter.js
+++ solutions/7-filter.js
@@ -1,3 +1,8 @@
 // BEGIN
-
+const getGirlFriends = (arr) => {
+    const girls = [];
+    arr.map((person) => girls.push(person.friends.filter((friend) => friend.gender === "female")));
+    return girls.flat(Infinity);
+}
+export default getGirlFriends;
 // END
