2d9060bc197cd608af1536d04570e208ccb2e511
dima55279 <dima55279@yandex.ru>
2025-03-21 20:56:15 +0300

    Выполнено задание 6.

--- solutions/6-map.js
+++ solutions/6-map.js
@@ -1,3 +1,10 @@
 // BEGIN
-
+const getChildren = (arr) => {
+    const children = [];
+    arr.map((person) => {
+        children.push(person.children);
+    });
+    return children.flat(Infinity);
+}
+export default getChildren;
 // END
