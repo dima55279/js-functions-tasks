0873feb50c72eabeb1ea7d9535e7b9eb62817e34
dima55279 <dima55279@yandex.ru>
2025-03-21 21:15:31 +0300

    Выполнено задание 8.

--- solutions/8-reduce.js
+++ solutions/8-reduce.js
@@ -1,3 +1,12 @@
 // BEGIN
-
+const groupBy = (arr, prop) => {
+    const fn = (acc, element) => {
+        if (!Object.hasOwn(acc, element[prop]))
+            acc[element[prop]] = [];
+        acc[element[prop]].push(element);
+        return acc;
+    }
+    return arr.reduce(fn, {});
+}
+export default groupBy;
 // END
