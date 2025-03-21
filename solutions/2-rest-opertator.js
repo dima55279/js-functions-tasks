710477b8731eb1c5f119f487de63e71eeb58cd28
dima55279 <dima55279@yandex.ru>
2025-03-21 13:48:35 +0300

    Выполнено задание 2.

--- solutions/2-rest-opertator.js
+++ solutions/2-rest-opertator.js
@@ -1,5 +1,10 @@
 import _ from 'lodash';
 
 // BEGIN
-
+const average = (...numbers) => {
+    if (numbers.length === 0)
+        return null;
+    return _.sum(numbers) / numbers.length;
+}
+export default average;
 // END
