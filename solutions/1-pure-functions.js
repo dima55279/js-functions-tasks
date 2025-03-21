bb5e9babc68b727a9d7f361b44c491ad5e3b0519
dima55279 <dima55279@yandex.ru>
2025-03-21 13:45:21 +0300

    Выполнено задание 1.

--- solutions/1-pure-functions.js
+++ solutions/1-pure-functions.js
@@ -1,3 +1,22 @@
 // BEGIN
-
+const checkPrime = (num) => {
+    let i = 2;
+    if (num <= 1) {
+        return false;
+    }
+    while (i <= Math.round(Math.sqrt(num))) {
+        if (num % i === 0) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+};
+const sayPrimeOrNot = (num) => {
+    if (checkPrime(num) === true) {
+        return console.log('yes');
+    }
+    return console.log('no');
+}
+export default sayPrimeOrNot;
 // END
