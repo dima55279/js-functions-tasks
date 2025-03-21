9e4dd45c0659a726613992b43624d5c1f35bb5f9
dima55279 <dima55279@yandex.ru>
2025-03-21 20:20:46 +0300

    Выполнено задание 3.

--- solutions/3-spread-operator.js
+++ solutions/3-spread-operator.js
@@ -1,3 +1,12 @@
 // BEGIN
-
+const convert = (...dates) => {
+    const newDates = []
+    dates.map((date) => {
+        const [year, month, day] = date;
+        const tempDate = new Date(year, month, day);
+        newDates.push(tempDate.toDateString())
+    });
+    return newDates;
+}
+export default convert;
 // END
