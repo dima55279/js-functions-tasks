5e6498a3aa22b0887bc03c9b5d7b465eab673188
dima55279 <dima55279@yandex.ru>
2025-03-21 20:48:32 +0300

    Выполнено задание 5.

--- solutions/5-high-order-functions.js
+++ solutions/5-high-order-functions.js
@@ -1,5 +1,15 @@
 import _ from 'lodash';
 
 // BEGIN
-
+const takeOldest = (arr, numbers = 1) => {
+    const oldestPerson = [];
+    const temp = _.sortBy(arr, (person) => Date.parse(person.birthday));
+    temp.map((person) => {
+        if (oldestPerson.length < numbers) {
+            oldestPerson.push(person);
+        }
+    })
+    return oldestPerson;
+}
+export default takeOldest;
 // END
