c9d4a618010136985ee87bd5f0fd2ebc6714c1a2
dima55279 <dima55279@yandex.ru>
2025-03-22 19:17:28 +0300

    Выполнено задание 9.

--- solutions/9-signals.js
+++ solutions/9-signals.js
@@ -8,5 +8,17 @@ const freeEmailDomains = [
 ];
 
 // BEGIN
-
+const getFreeDomainsCount = (arr) => {
+  const countOfEmails = {};
+  if (arr.filter((email) => email.includes(freeEmailDomains[0])).length > 0)
+    countOfEmails[freeEmailDomains[0]] = arr.filter((email) => email.includes(freeEmailDomains[0])).length;
+  if (arr.filter((email) => email.includes(freeEmailDomains[1])).length > 0)
+    countOfEmails[freeEmailDomains[1]] = arr.filter((email) => email.includes(freeEmailDomains[1])).length;
+  if (arr.filter((email) => email.includes(freeEmailDomains[2])).length > 0)
+    countOfEmails[freeEmailDomains[2]] = arr.filter((email) => email.includes(freeEmailDomains[2])).length;
+  if (arr.filter((email) => email.includes(freeEmailDomains[3])).length > 0)
+    countOfEmails[freeEmailDomains[3]] = arr.filter((email) => email.includes(freeEmailDomains[3])).length;
+  return countOfEmails;
+}
+export default getFreeDomainsCount;
 // END
