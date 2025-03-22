35d1c74b49520ceb3497154b86febc5aad5800b3
dima55279 <dima55279@yandex.ru>
2025-03-22 20:04:00 +0300

    Выполнено задание 11. Закончена ЛР 4.

--- solutions/11-iterative.js
+++ solutions/11-iterative.js
@@ -1,6 +1,15 @@
 const smallestDivisor = (num) => {
     // BEGIN
-
+    if (num === 1) {
+        return 1;
+    }
+    const iter = (counter) => {
+        if (num % counter === 0) {
+            return counter;
+        }
+        return iter(counter + 1);
+    }
+    return iter(2)
     // END
   };
   
