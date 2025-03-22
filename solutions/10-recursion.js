6397e846bc7a4558f4458707f072b9f374f2084a
dima55279 <dima55279@yandex.ru>
2025-03-22 19:22:06 +0300

    Выполнено задание 10.

--- solutions/10-recursion.js
+++ solutions/10-recursion.js
@@ -1,6 +1,10 @@
 const sequenceSum = (begin, end) => {
     // BEGIN
-
+    if (begin > end)
+      return NaN;
+    else if (begin === end)
+      return begin;
+    else return begin + sequenceSum(begin + 1, end);
     // END
   };
   
